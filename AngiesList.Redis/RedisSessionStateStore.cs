5ebf09261944e743dd31bccd0841263871285938
Nathan Davis <nathwdavis@yahoo.com>
2011-11-16 16:09:49 -0500

    one-time big reformat and cleanup

--- AngiesList.Redis/RedisSessionStateStore.cs
+++ AngiesList.Redis/RedisSessionStateStore.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Web.SessionState;
@@ -9,7 +8,6 @@ using System.Web;
 using System.Web.Configuration;
 using BookSleeve;
 using System.IO;
-using System.Diagnostics;
 
 namespace AngiesList.Redis
 {
@@ -17,189 +15,185 @@ namespace AngiesList.Redis
 	{
 		private RedisConnection redisConnection;
 		private SessionStateSection sessionStateConfig;
-        private string host;
-        private int port;
-        private string lockHashKey;
-        private readonly object locker = new {};
+		private string host;
+		private int port;
+		private string lockHashKey;
+		private readonly object locker = new { };
 
-		public override void Initialize (string name, NameValueCollection config)
+		public override void Initialize(string name, NameValueCollection config)
 		{
-			if (String.IsNullOrWhiteSpace (name)) {
+			if (String.IsNullOrWhiteSpace(name)) {
 				name = "AspNetSession";
 			}
-			base.Initialize (name, config);
-			
+			base.Initialize(name, config);
+
 			lockHashKey = name + ":LockedSessions";
-			
-			sessionStateConfig = (SessionStateSection)WebConfigurationManager.GetSection ("system.web/sessionState");
+
+			sessionStateConfig = (SessionStateSection)WebConfigurationManager.GetSection("system.web/sessionState");
 			var stateConnection = sessionStateConfig.StateConnectionString;
-			
-			if (!String.IsNullOrWhiteSpace (stateConnection)) {
-				var stateConnectionParts = sessionStateConfig.StateConnectionString.Split ('=', ':');
-                host = stateConnectionParts.ElementAtOrDefault(1) ?? "localhost";
-				var portAsString = stateConnectionParts.ElementAtOrDefault (2) ?? "6379";
-				port = Int32.Parse (portAsString);
-				
-			} else {
-                host = "localhost";
-                port = 6379;
+
+			if (!String.IsNullOrWhiteSpace(stateConnection)) {
+				var stateConnectionParts = sessionStateConfig.StateConnectionString.Split('=', ':');
+				host = stateConnectionParts.ElementAtOrDefault(1) ?? "localhost";
+				var portAsString = stateConnectionParts.ElementAtOrDefault(2) ?? "6379";
+				port = Int32.Parse(portAsString);
+
+			}
+			else {
+				host = "localhost";
+				port = 6379;
 			}
 		}
 
-        private RedisConnection GetRedisConnection()
-        {
-            if (redisConnection == null ||
-                (redisConnection.State != RedisConnectionBase.ConnectionState.Open &&
-                 redisConnection.State != RedisConnectionBase.ConnectionState.Opening))
-            {
-                lock (locker)
-                {
-                    if (redisConnection == null ||
-                        (redisConnection.State != RedisConnectionBase.ConnectionState.Open &&
-                         redisConnection.State != RedisConnectionBase.ConnectionState.Opening))
-                    {
-                        redisConnection = new RedisConnection(host, port);
-                        redisConnection.Closed += (object sender, EventArgs e) =>
-                        {
-                            //Debug.WriteLine("redisConnection closed");
-                        };
-                        redisConnection.Open();
-                    }
-                }
-            }
-            return redisConnection;
-        }
-
-		public override bool SetItemExpireCallback (SessionStateItemExpireCallback expireCallback)
+		private RedisConnection GetRedisConnection()
 		{
-			return false;
+			if (redisConnection == null ||
+				 (redisConnection.State != RedisConnectionBase.ConnectionState.Open &&
+				  redisConnection.State != RedisConnectionBase.ConnectionState.Opening)) {
+				lock (locker) {
+					if (redisConnection == null ||
+						 (redisConnection.State != RedisConnectionBase.ConnectionState.Open &&
+						  redisConnection.State != RedisConnectionBase.ConnectionState.Opening)) {
+						redisConnection = new RedisConnection(host, port);
+						redisConnection.Closed += (object sender, EventArgs e) => {
+							//Debug.WriteLine("redisConnection closed");
+						};
+						redisConnection.Open();
+					}
+				}
+			}
+			return redisConnection;
 		}
 
-		public override void SetAndReleaseItemExclusive (HttpContext context,
-          string id,
-          SessionStateStoreData item,
-          object lockId,
-          bool newItem)
+		private string GetKeyForSessionId(string id)
 		{
-            var redis = GetRedisConnection();
-            var getLock = redis.Hashes.Get(0, lockHashKey, id);
+			return this.Name + ":" + id;
+		}
+
+		public override void SetAndReleaseItemExclusive(HttpContext context,
+			 string id,
+			 SessionStateStoreData item,
+			 object lockId,
+			 bool newItem)
+		{
+			var redis = GetRedisConnection();
+			var getLock = redis.Hashes.Get(0, lockHashKey, id);
 			var lockIdAsBytes = (byte[])lockId;
-			var ms = new MemoryStream ();
-			var writer = new BinaryWriter (ms);
+			var ms = new MemoryStream();
+			var writer = new BinaryWriter(ms);
 
-			if (item.Items as SessionStateItemCollection != null)
-				((SessionStateItemCollection)item.Items).Serialize (writer);
-			
-			writer.Close ();
+			if (item.Items as SessionStateItemCollection != null) {
+				((SessionStateItemCollection)item.Items).Serialize(writer);
+			}
 
-			byte[] sessionData = ms.ToArray ();
-			var sessionItemHash = new Dictionary<string, byte[]> ();
-            sessionItemHash.Add("initialize", new byte[] { 0 });
-            sessionItemHash.Add("data", sessionData);
-            sessionItemHash.Add("timeoutMinutes", BitConverter.GetBytes(item.Timeout));
-			
+			writer.Close();
+
+			byte[] sessionData = ms.ToArray();
+			var sessionItemHash = new Dictionary<string, byte[]>();
+			sessionItemHash.Add("initialize", new byte[] { 0 });
+			sessionItemHash.Add("data", sessionData);
+			sessionItemHash.Add("timeoutMinutes", BitConverter.GetBytes(item.Timeout));
+
 			LockData lockData;
 			getLock.Wait();
 			if (getLock.Result == null) {
-				redis.Hashes.Set (0, GetKeyForSessionId (id), sessionItemHash, false);
+				redis.Hashes.Set(0, GetKeyForSessionId(id), sessionItemHash, false);
 			}
 			else if (LockData.TryParse(getLock.Result, out lockData) && Enumerable.SequenceEqual(lockData.LockId, lockIdAsBytes)) {
-				redis.Hashes.Set (0, GetKeyForSessionId (id), sessionItemHash, false);
-				redis.Hashes.Remove (0, lockHashKey, id);
+				redis.Hashes.Set(0, GetKeyForSessionId(id), sessionItemHash, false);
+				redis.Hashes.Remove(0, lockHashKey, id);
 			}
 		}
 
-		public override SessionStateStoreData GetItem (HttpContext context, 
-            string id, 
-            out bool locked, 
-            out TimeSpan lockAge, 
-            out object lockId, 
-            out SessionStateActions actions)
+		public override SessionStateStoreData GetItem(HttpContext context,
+				string id,
+				out bool locked,
+				out TimeSpan lockAge,
+				out object lockId,
+				out SessionStateActions actions)
 		{
-            var redis = GetRedisConnection();
-            var getSessionData = redis.Hashes.GetAll (0, GetKeyForSessionId (id));
+			var redis = GetRedisConnection();
+			var getSessionData = redis.Hashes.GetAll(0, GetKeyForSessionId(id));
 			locked = false;
-			lockAge = new TimeSpan (0);
+			lockAge = new TimeSpan(0);
 			lockId = null;
 			actions = SessionStateActions.None;
-			
+
 			if (getSessionData.Result == null) {
 				return null;
-			} else {
-                var sessionItems = new SessionStateItemCollection();
-                var sessionDataHash = getSessionData.Result;
-                if (sessionDataHash.Count == 3)
-                {
-                    var ms = new MemoryStream(sessionDataHash["data"]);
-                    if (ms.Length > 0)
-                    {
-                        var reader = new BinaryReader(ms);
-                        sessionItems = SessionStateItemCollection.Deserialize(reader);
-                    }
+			}
+			else {
+				var sessionItems = new SessionStateItemCollection();
+				var sessionDataHash = getSessionData.Result;
+				if (sessionDataHash.Count == 3) {
+					var ms = new MemoryStream(sessionDataHash["data"]);
+					if (ms.Length > 0) {
+						var reader = new BinaryReader(ms);
+						sessionItems = SessionStateItemCollection.Deserialize(reader);
+					}
 
-                    var timeoutMinutes = BitConverter.ToInt32(sessionDataHash["timeoutMinutes"], 0);
-                    redis.Keys.Expire(0, GetKeyForSessionId(id), timeoutMinutes * 60);
-                }
-				
-                return new SessionStateStoreData (sessionItems,
-                    SessionStateUtility.GetSessionStaticObjects (context),
-                    (int)sessionStateConfig.Timeout.TotalMinutes);
+					var timeoutMinutes = BitConverter.ToInt32(sessionDataHash["timeoutMinutes"], 0);
+					redis.Keys.Expire(0, GetKeyForSessionId(id), timeoutMinutes * 60);
+				}
+
+				return new SessionStateStoreData(sessionItems,
+					 SessionStateUtility.GetSessionStaticObjects(context),
+					 (int)sessionStateConfig.Timeout.TotalMinutes);
 			}
 		}
 
-		public override SessionStateStoreData GetItemExclusive (HttpContext context, 
-            string id, 
-            out bool locked, 
-            out TimeSpan lockAge, 
-            out object lockId, 
-            out SessionStateActions actions)
+		public override SessionStateStoreData GetItemExclusive(HttpContext context,
+				string id,
+				out bool locked,
+				out TimeSpan lockAge,
+				out object lockId,
+				out SessionStateActions actions)
 		{
-            var redis = GetRedisConnection();
-            var getLockData = redis.Hashes.Get (0, lockHashKey, id);
-			
+			var redis = GetRedisConnection();
+			var getLockData = redis.Hashes.Get(0, lockHashKey, id);
+
 			actions = SessionStateActions.None;
 			locked = false;
 			lockId = null;
 			lockAge = TimeSpan.MinValue;
-			
+
 			getLockData.Wait();
 			var rawLockData = getLockData.Result;
-			
+
 			if (rawLockData == null) {
-				var lockData = LockData.New ();
+				var lockData = LockData.New();
 				using (var trans = redis.CreateTransaction()) {
-                    var setLock = trans.Hashes.SetIfNotExists(0, lockHashKey, id, lockData.ToByteArray());
-                    var getSessionData = trans.Hashes.GetAll(0, GetKeyForSessionId(id));
-					trans.Execute ();
+					var setLock = trans.Hashes.SetIfNotExists(0, lockHashKey, id, lockData.ToByteArray());
+					var getSessionData = trans.Hashes.GetAll(0, GetKeyForSessionId(id));
+					trans.Execute();
 					setLock.Wait();
 					if (setLock.Result) {
 						locked = true;
-						lockAge = new TimeSpan (0);
+						lockAge = new TimeSpan(0);
 						lockId = lockData.LockId;
 
-                        var sessionItems = new SessionStateItemCollection();
-                        var sessionDataHash = redis.Wait(getSessionData);
-                        if (sessionDataHash.Count == 3)
-                        {
-                            actions = sessionDataHash["initialize"][0] == 1 ?
-                                    SessionStateActions.InitializeItem : SessionStateActions.None;
+						var sessionItems = new SessionStateItemCollection();
+						var sessionDataHash = redis.Wait(getSessionData);
+						if (sessionDataHash.Count == 3) {
+							actions = sessionDataHash["initialize"][0] == 1 ?
+									  SessionStateActions.InitializeItem : SessionStateActions.None;
 
-                            var ms = new MemoryStream(sessionDataHash["data"]);
-                            if (ms.Length > 0)
-                            {
-                                var reader = new BinaryReader(ms);
-                                sessionItems = SessionStateItemCollection.Deserialize(reader);
-                            }
+							var ms = new MemoryStream(sessionDataHash["data"]);
+							if (ms.Length > 0) {
+								var reader = new BinaryReader(ms);
+								sessionItems = SessionStateItemCollection.Deserialize(reader);
+							}
 
-                            var timeoutMinutes = BitConverter.ToInt32(sessionDataHash["timeoutMinutes"], 0);
-                            redis.Keys.Expire(0, GetKeyForSessionId(id), timeoutMinutes * 60);
-                        }
-						return new SessionStateStoreData (sessionItems,
-		                    SessionStateUtility.GetSessionStaticObjects (context),
-		                    (int)sessionStateConfig.Timeout.TotalMinutes); 
-					} else {
+							var timeoutMinutes = BitConverter.ToInt32(sessionDataHash["timeoutMinutes"], 0);
+							redis.Keys.Expire(0, GetKeyForSessionId(id), timeoutMinutes * 60);
+						}
+						return new SessionStateStoreData(sessionItems,
+								  SessionStateUtility.GetSessionStaticObjects(context),
+								  (int)sessionStateConfig.Timeout.TotalMinutes);
+					}
+					else {
 						rawLockData = redis.Hashes.Get(0, lockHashKey, id).Result;
-						if (rawLockData != null && LockData.TryParse (rawLockData, out lockData)) {
+						if (rawLockData != null && LockData.TryParse(rawLockData, out lockData)) {
 							locked = true;
 							lockId = lockData.LockId;
 							lockAge = DateTime.UtcNow - lockData.LockUtcTime;
@@ -207,132 +201,131 @@ namespace AngiesList.Redis
 						return null;
 					}
 				}
-			} else {
+			}
+			else {
 				LockData lockData;
-				if (LockData.TryParse (rawLockData, out lockData)) {
+				if (LockData.TryParse(rawLockData, out lockData)) {
 					locked = true;
 					lockId = lockData.LockId;
 					lockAge = DateTime.UtcNow - lockData.LockUtcTime;
 				}
-				//Big FAIL
 				return null;
 			}
 		}
-		
+
 		internal struct LockData
 		{
-			static readonly byte SEPERATOR = Encoding.ASCII.GetBytes (";") [0];
-			
-			public static LockData New ()
+			static readonly byte SEPERATOR = Encoding.ASCII.GetBytes(";")[0];
+
+			public static LockData New()
 			{
-				var data = new LockData ();
-				data.LockId = Guid.NewGuid ().ToByteArray ();
+				var data = new LockData();
+				data.LockId = Guid.NewGuid().ToByteArray();
 				data.LockUtcTime = DateTime.UtcNow;
 				return data;
 			}
 
-			public static bool TryParse (byte[] raw, out LockData data)
+			public static bool TryParse(byte[] raw, out LockData data)
 			{
 				if (raw != null && raw.Length > 1) {
 					var lockId = raw.Take(16).ToArray();
 					var lockTicks = BitConverter.ToInt64(raw, 17);
 					data = new LockData {
 						LockId = lockId,
-						LockUtcTime = new DateTime (lockTicks)
+						LockUtcTime = new DateTime(lockTicks)
 					};
 					return true;
 				}
-				data = new LockData ();
+				data = new LockData();
 				return false;
 			}
 
 			public byte[] LockId;
 			public DateTime LockUtcTime;
 
-			public override string ToString ()
+			public override string ToString()
 			{
-				return BitConverter.ToString (LockId) + ";" + LockUtcTime.Ticks;
+				return BitConverter.ToString(LockId) + ";" + LockUtcTime.Ticks;
 			}
 
-			public byte[] ToByteArray ()
+			public byte[] ToByteArray()
 			{
-				return LockId.Concat (new byte[] {SEPERATOR}.Concat (BitConverter.GetBytes (LockUtcTime.Ticks))).ToArray ();
+				return LockId.Concat(new byte[] { SEPERATOR }.Concat(BitConverter.GetBytes(LockUtcTime.Ticks))).ToArray();
 			}
 		}
 
-		public override SessionStateStoreData CreateNewStoreData (HttpContext context, int timeout)
+		public override SessionStateStoreData CreateNewStoreData(HttpContext context, int timeout)
 		{
-			return new SessionStateStoreData (
-                new SessionStateItemCollection (), 
-                SessionStateUtility.GetSessionStaticObjects (context), 
-                timeout
-            );
+			return new SessionStateStoreData(
+					 new SessionStateItemCollection(),
+					 SessionStateUtility.GetSessionStaticObjects(context),
+					 timeout
+				);
 		}
 
-		public override void CreateUninitializedItem (HttpContext context, string id, int timeout)
+		public override void CreateUninitializedItem(HttpContext context, string id, int timeout)
 		{
-            var redis = GetRedisConnection();
-            var ms = new MemoryStream ();
-			var writer = new BinaryWriter (ms);
-			(new SessionStateItemCollection ()).Serialize (writer);
-			writer.Close ();
-			byte[] sessionData = ms.ToArray ();
-			var newItemHash = new Dictionary<string, byte[]> ();
-            newItemHash.Add("data", sessionData);
-            newItemHash.Add("initialize", new byte[] { 0 });
-            newItemHash.Add("timeoutMinutes", BitConverter.GetBytes(timeout));
-			redis.Hashes.Set (0, GetKeyForSessionId (id), newItemHash, false);
-            redis.Keys.Expire(0, GetKeyForSessionId(id), timeout * 60);
+			var redis = GetRedisConnection();
+			var ms = new MemoryStream();
+			var writer = new BinaryWriter(ms);
+			(new SessionStateItemCollection()).Serialize(writer);
+			writer.Close();
+			byte[] sessionData = ms.ToArray();
+			var newItemHash = new Dictionary<string, byte[]>();
+			newItemHash.Add("data", sessionData);
+			newItemHash.Add("initialize", new byte[] { 0 });
+			newItemHash.Add("timeoutMinutes", BitConverter.GetBytes(timeout));
+			redis.Hashes.Set(0, GetKeyForSessionId(id), newItemHash, false);
+			redis.Keys.Expire(0, GetKeyForSessionId(id), timeout * 60);
 		}
 
-		public override void Dispose ()
+		public override void Dispose()
 		{
-            var redis = GetRedisConnection();
-            IDisposable disposable;
+			var redis = GetRedisConnection();
+			IDisposable disposable;
 			if ((disposable = redis as IDisposable) != null) {
-				disposable.Dispose ();
+				disposable.Dispose();
 			}
 		}
 
-		public override void InitializeRequest (HttpContext context)
+		public override void ReleaseItemExclusive(HttpContext context, string id, object lockId)
 		{
-		}
-
-		public override void EndRequest (HttpContext context)
-		{
-		}
-
-		public override void ReleaseItemExclusive (HttpContext context, string id, object lockId)
-		{
-            var redis = GetRedisConnection();
-            //var getLock = redis.Hashes.Get(0, lockHashKey, id);
+			var redis = GetRedisConnection();
+			//var getLock = redis.Hashes.Get(0, lockHashKey, id);
 			//var lockIdAsString = (string)lockId;
 			//if (getLock.Result && getLock.Result == lockIdAsString) {
-				redis.Hashes.Remove (0, lockHashKey, id);
+			redis.Hashes.Remove(0, lockHashKey, id);
 			//}
 		}
 
-		public override void RemoveItem (HttpContext context, string id, object lockId, SessionStateStoreData item)
+		public override void RemoveItem(HttpContext context, string id, object lockId, SessionStateStoreData item)
 		{
-            var redis = GetRedisConnection();
-            var getLock = redis.Hashes.Get(0, lockHashKey, id);
+			var redis = GetRedisConnection();
+			var getLock = redis.Hashes.Get(0, lockHashKey, id);
 			var lockIdAsBytes = (byte[])lockId;
-            LockData lockData;
-            if (getLock.Result != null && LockData.TryParse(getLock.Result, out lockData) && lockData.LockId == lockIdAsBytes)
-            {
-				redis.Keys.Remove (0, GetKeyForSessionId (id));
-				redis.Hashes.Remove (0, lockHashKey, id);
+			LockData lockData;
+			if (getLock.Result != null && LockData.TryParse(getLock.Result, out lockData) && lockData.LockId == lockIdAsBytes) {
+				redis.Keys.Remove(0, GetKeyForSessionId(id));
+				redis.Hashes.Remove(0, lockHashKey, id);
 			}
 		}
-		
-		public override void ResetItemTimeout (HttpContext context, string id)
+
+		public override bool SetItemExpireCallback(SessionStateItemExpireCallback expireCallback)
+		{
+			return false;
+		}
+
+		public override void ResetItemTimeout(HttpContext context, string id)
 		{
 			//TODO
 		}
-			
-		private string GetKeyForSessionId (string id)
+
+		public override void InitializeRequest(HttpContext context)
 		{
-			return this.Name + ":" + id;	
+		}
+
+		public override void EndRequest(HttpContext context)
+		{
 		}
 	}
 }
